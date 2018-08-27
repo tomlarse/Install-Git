45fbcc8a0482aa074f4e81ad8308a148e23c474f
Tom-Inge Larsen <tom-inge.larsen@advania.no>
2018-08-27 10:39:58 +0200

    added dotsourcing to tests

--- Tests/Install-Git.Tests.ps1
+++ Tests/Install-Git.Tests.ps1
@@ -8,7 +8,7 @@ Describe 'File Tests' {
     }
 }
 
-$ScriptFilePath
+. $ScriptFilePath
 
 # reload the PATH
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
